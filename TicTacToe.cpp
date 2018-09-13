3bdfc88b6fd00a45c3810010b866a6f1dbbdeb22
U-BSD\357538 <357538@SHS-10L74C39B.beaverton.k12.or.us>
2018-09-13 09:15:31 -0700

    Third Commit

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
-#include <cstring>
+#include <string.h>
 
 using namespace std;
 
 void clearBoard(char* Ptr[3][3]);
 void drawBoard(char* Ptr[3][3]);
+bool checkWin(char* Ptr[3][3], char player);
 
 int main()
 {
-
   char board[3][3];
   char* boardPtr[3][3];
+  char input[2];
+  char player = 'X';
   for(int x = 0; x < 3; x++){
     for(int y = 0; y < 3; y++){
       boardPtr[x][y] = &board[x][y];
@@ -18,6 +20,24 @@ int main()
   }
   clearBoard(boardPtr);
   drawBoard(boardPtr);
+  while(!checkWin(boardPtr, player)){
+    cin.get(input, 3); // Do With do while
+    cin.clear();
+    cin.ignore(100, '\n');
+    cout << input << endl;
+    if(input[0] == '1' || input[0] == '2' || input[0] == '3'){
+      if(input[1] == 'a' || input[1] == 'b' || input[1] == 'c'){
+	if(board[(int)input[0] - '1'][(int)input[1] - 'a'] == ' '){
+	  board[(int)input[0] - '1'][(int)input[1] - 'a'] = player;
+	}
+      }
+    }
+    else{
+      cout << "Fskdfjhskdfh" << endl;
+    }
+    drawBoard(boardPtr);
+    player == 'Y';
+  }
 }
 
 void clearBoard(char* Ptr[3][3]){
@@ -47,4 +67,26 @@ void drawBoard(char* Ptr[3][3]){
     cout << " " << *Ptr[2][i];
   }
   cout << endl;  
+}
+
+bool checkWin(char* Ptr[3][3], char player){
+  for(int i = 0; i < 3; i++){
+    if(*Ptr[0][i] == player && *Ptr[1][i] == player && *Ptr[2][i] == player){
+      cout << "A" << endl;
+      return true;
+    }
+    if(*Ptr[i][0] == player && *Ptr[i][1] == player && *Ptr[i][2] == player){
+      cout << "B" << endl;
+      return true;
+    }
+  }
+  if(*Ptr[0][0] == player && *Ptr[1][1] == player && *Ptr[2][2] == player){
+    cout << "C" << endl;
+    return true;
+  }
+  if(*Ptr[2][0] == player && *Ptr[1][1] == player && *Ptr[0][2] == player){
+    return true;
+    cout << "D" << endl;
+  }
+  return false;
 }
