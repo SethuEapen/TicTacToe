235adc4016f64a4e5a8d98ea76644e24c6eb6b2c
U-BSD\357538 <357538@SHS-10L74C39B.beaverton.k12.or.us>
2018-09-17 09:11:54 -0700

    Sixth Commit

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -6,6 +6,7 @@ using namespace std;
 void clearBoard(char* Ptr[3][3]);
 void drawBoard(char* Ptr[3][3]);
 bool checkWin(char* Ptr[3][3], char player);
+bool checkTie(char* Ptr[3][3]);
 
 int main()
 {
@@ -13,45 +14,47 @@ int main()
   char* boardPtr[3][3];
   char input[2];
   char player = 'O';
+  char stillPlaying = 'y';
   for(int x = 0; x < 3; x++){
     for(int y = 0; y < 3; y++){
       boardPtr[x][y] = &board[x][y];
     }
   }
-  clearBoard(boardPtr);
-  drawBoard(boardPtr);
-  while(!checkWin(boardPtr, player)){
-    if(player == 'X'){
-      player = 'O';
-    }
-    else{
-      player = 'X';
-    }
-    cin.get(input, 3); // Do With do while
-    cin.clear();
-    cin.ignore(100, '\n');
-    cout << input << endl;
-    if(input[0] == '1' || input[0] == '2' || input[0] == '3'){
-      if(input[1] == 'a' || input[1] == 'b' || input[1] == 'c'){
-	if(board[(int)input[1] - 'a'][(int)input[0] - '1'] == ' '){
-	  board[(int)input[1] - 'a'][(int)input[0] - '1'] = player;
-	}
-	else{
-	  cout << "Fskdfjhskdfh" << endl;
-	}
+  while(stillPlaying == 'y'){
+    clearBoard(boardPtr);
+    drawBoard(boardPtr);
+    player = 'O';
+    while(!checkWin(boardPtr, player) && !checkTie(boardPtr)){
+      if(player == 'X'){
+	player = 'O';
       }
       else{
-	cout << "Fskdfjhskdfh" << endl;
+	player = 'X';
       }
+      do
+      {
+	cin.get(input, 3);
+	cin.clear();
+	cin.ignore(100, '\n');
+      }while(!(input[0] == '1' || input[0] == '2' || input[0] == '3')
+	     && !(input[1] == 'a' || input[1] == 'b' || input[1] =='c'));
+      if(board[(int)input[1] - 'a'][(int)input[0] - '1'] == ' '){
+	board[(int)input[1] - 'a'][(int)input[0] - '1'] = player;
+      }
+      drawBoard(boardPtr);
+    }
+    if(checkTie(boardPtr)){
+      cout << "Tie game!" << endl;
     }
     else{
-      cout << "Fskdfjhskdfh" << endl;
+      cout << "Congratulations! Player " << player << " won!" << endl;
     }
-    drawBoard(boardPtr);
+    cout << "Do you want to play again?(y or n)" << endl;
+    cin >> stillPlaying;
+    cin.clear();
+    cin.ignore(100, '\n');
   }
-  cout << "Congratulations! Player " << player << " won!" << endl << "Do you want to play again?(y or n)" << endl;
 }
-
 void clearBoard(char* Ptr[3][3]){
   for(int x = 0; x < 3; x++){
     for(int y = 0; y < 3; y++){
@@ -79,13 +82,6 @@ void drawBoard(char* Ptr[3][3]){
     cout << ' ' << *Ptr[2][i];
   }
   cout << endl;
-  
-  for(int x = 0; x < 3; x++){
-    for(int y = 0; y < 3; y++){
-      cout << endl;
-      cout << *Ptr[x][y];
-    }
-  }
   cout << endl;  
 }
 
@@ -109,4 +105,15 @@ bool checkWin(char* Ptr[3][3], char player){
     cout << "D" << endl;
   }
   return false;
+}
+
+bool checkTie(char* Ptr[3][3]){
+  for(int x = 0; x < 3; x++){
+    for(int y = 0; y < 3; y++){
+      if(*Ptr[x][y] == ' '){
+	return false;
+      }
+    }
+  }
+  return true;
 }
